23c1234fee1731a207aa0622ea120a5beb88d8be
Juan Cristobal Pazos <juancri@lalegion.cl>
2015-05-25 20:21:36 -0300

    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 User.destroy_all
 Post.destroy_all
 Comment.destroy_all
+
+posts = []
+comments = []
 
 users = User.create([
   { name: 'Juan Cristobal Pazos', email: 'juancri@pazos.com', password: 'password', password_confirmation: 'password', role: 1 },
@@ -16,22 +11,10 @@ users = User.create([
   { name: 'Macarena Standen', email: 'maca@standen.com', password: 'password', password_confirmation: 'password', role: 0 }
 ])
 
-posts = Post.create([
-  { title: Faker::Lorem.sentence(3, true, 4), content: Faker::Lorem.paragraph(20, true, 10), user: users.first },
-  { title: Faker::Lorem.sentence(3, true, 4), content: Faker::Lorem.paragraph(30, true, 20), user: users.last },
-  { title: Faker::Lorem.sentence(3, true, 4), content: Faker::Lorem.paragraph(20, true, 15), user: users.last },
-  { title: Faker::Lorem.sentence(3, true, 4), content: Faker::Lorem.paragraph(40, true, 25), user: users[1] }
-])
+5.times do |post|
+ posts << Post.create(title: Faker::Lorem.sentence(3, true, 4), content: Faker::Lorem.paragraph(20, true, 10), user: users.sample)
+end
 
-comments = Comment.create([
-  { content: Faker::Lorem.paragraph(2), post: posts.first, user: users.last },
-  { content: Faker::Lorem.paragraph(3), post: posts.last, user: users.first },
-  { content: Faker::Lorem.paragraph(1), post: posts.first, user: users.first },
-  { content: Faker::Lorem.paragraph(3), post: posts.first, user: users[1] },
-  { content: Faker::Lorem.paragraph(4), post: posts[1], user: users.last },
-  { content: Faker::Lorem.paragraph(3), post: posts[1], user: users[1] },
-  { content: Faker::Lorem.paragraph(1), post: posts[2], user: users[1] },
-  { content: Faker::Lorem.paragraph(2), post: posts[2], user: users.last },
-  { content: Faker::Lorem.paragraph(3), post: posts.last, user: users[1] },
-  { content: Faker::Lorem.paragraph(2), post: posts.last, user: users[2] }
-])
+20.times do |comment|
+  comments << Comment.create(content: Faker::Lorem.paragraph(2), post: posts.sample, user: users.sample)
+end
