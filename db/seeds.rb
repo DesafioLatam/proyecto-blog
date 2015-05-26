57e2543ef1fca5b542aaea6103b999e109623504
Juan Cristobal Pazos <juancri@lalegion.cl>
2015-05-26 19:27:46 -0300

    edited seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.destroy_all
+seeUser.destroy_all
 Post.destroy_all
 Comment.destroy_all
 
@@ -6,15 +6,37 @@ posts = []
 comments = []
 
 users = User.create([
-  { name: 'Juan Cristobal Pazos', email: 'juancri@pazos.com', password: 'password', password_confirmation: 'password', role: 1 },
-  { name: 'Gonzalo Sanchez', email: 'gonzalo@sanchez.com', password: 'password', password_confirmation: 'password', role: 0 },
-  { name: 'Macarena Standen', email: 'maca@standen.com', password: 'password', password_confirmation: 'password', role: 0 }
+  {
+    name: 'Juan Cristobal Pazos',
+    email: 'juancri@pazos.com',
+    password: 'password',
+    password_confirmation: 'password',
+    role: 1
+  },
+  {
+    name: 'Gonzalo Sanchez',
+    email: 'gonzalo@sanchez.com',
+    password: 'password',
+    password_confirmation: 'password',
+    role: 0
+  },
+  {
+    name: 'Macarena Standen',
+    email: 'maca@standen.com',
+    password: 'password',
+    password_confirmation: 'password',
+    role: 0
+  }
 ])
 
-5.times do |post|
- posts << Post.create(title: Faker::Lorem.sentence(3, true, 4), content: Faker::Lorem.paragraph(20, true, 10), user: users.sample)
+5.times do
+ posts << Post.create(title: Faker::Lorem.sentence(3, true, 4),
+                      content: Faker::Lorem.paragraph(20, true, 10),
+                      user: users.sample)
 end
 
-20.times do |comment|
-  comments << Comment.create(content: Faker::Lorem.paragraph(2), post: posts.sample, user: users.sample)
+20.times do
+  comments << Comment.create(content: Faker::Lorem.paragraph(2),
+                             post: posts.sample,
+                             user: users.sample)
 end
